b818925df90c0f332043d09c24a9ddd8f401a13f
perrettJ4 <perrettj4@gmail.com>
2021-09-09 19:34:18 +0100

    added cors library

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const scoresRouter = require("./routes/scores");
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
 app.use("/", indexRouter);
 app.use("/scores", scoresRouter);
 
